ed9fb11f1e19b9df56391b1cfc207d02d3d03491
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-11-27 14:13:54 +0100

    Start on Seed

---
+++ Companies.API/Data/SeedData.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Companies.API.Data
+{
+    public class SeedData
+    {
+        private static APIContext db;
+        internal static async Task InitAsync(APIContext context)
+        {
+            db = context ?? throw new ArgumentNullException(nameof(context));
+
+            if (await db.Company.AnyAsync()) return;
+        }
+    }
+}
---
+++ Companies.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Companies.API.Data;
+
+namespace Companies.API.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var db = serviceProvider.GetRequiredService<APIContext>();
+
+                //db.Database.EnsureDeleted();
+                //db.Database.Migrate();
+
+                try
+                {
+                    await SeedData.InitAsync(db);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+
+        }
+    }
+}
