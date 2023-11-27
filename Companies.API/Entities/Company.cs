8f5b6b943e1affa358c4ebe779fecdea47ec028b
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-11-27 13:48:17 +0100

    Entities

---
+++ Companies.API/Entities/Company.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Companies.API.Entities
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Company name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Company address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Address is 60 characters")]
+        public string Address { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
---
+++ Companies.API/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Companies.API.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Age is a required field.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Position is a required field.")]
+        [StringLength(maximumLength: 20, MinimumLength = 3, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string Position { get; set; } = string.Empty;
+    }
+}
