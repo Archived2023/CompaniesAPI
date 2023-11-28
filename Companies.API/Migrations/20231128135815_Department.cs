460780de6f56aa0e385a538d970c3f2feba4f3c9
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-11-28 14:58:28 +0100

    Add migration department

---
+++ Companies.API/Migrations/20231128135815_Department.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Companies.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Department : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentId",
+                table: "Employee",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Company",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentId",
+                table: "Employee",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Department_DepartmentId",
+                table: "Employee",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Department_DepartmentId",
+                table: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_DepartmentId",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Employee",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Company",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+    }
+}
