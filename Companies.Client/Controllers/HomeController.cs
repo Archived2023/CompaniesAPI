8bae9c4410abb87f6be2273dfc3b1ff93c1631e5
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-12-04 13:20:32 +0100

    GetWithRequestMessageAsync

--- Companies.Client/Controllers/HomeController.cs
+++ Companies.Client/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Companies.API.Dtos.CompaniesDtos;
 using Companies.Client.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace Companies.Client.Controllers
@@ -9,6 +10,7 @@ namespace Companies.Client.Controllers
     public class HomeController : Controller
     {
         private readonly HttpClient httpClient;
+        private const string json = "application/json";
 
         public HomeController()
         {
@@ -19,11 +21,34 @@ namespace Companies.Client.Controllers
         public async Task<IActionResult> Index()
         {
 
-            var res = await SimpleGetAsync();
-            var res2 = await SimpleGetAsync2();
+            //var res = await SimpleGetAsync();
+            //var res2 = await SimpleGetAsync2();
+            //var res3 = await GetWithRequestMessageAsync();
+            var res4 = await PostWithRequestMessageAsync();
 
 
             return View();
+        }
+
+        private async Task<CompanyDto> PostWithRequestMessageAsync()
+        {
+           
+        }
+
+        private async Task<IEnumerable<CompanyDto>> GetWithRequestMessageAsync()
+        {
+            var requst = new HttpRequestMessage(HttpMethod.Get, "api/companies");
+            requst.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+
+            var response = await httpClient.SendAsync(requst);
+            response.EnsureSuccessStatusCode();
+
+            var res = await response.Content.ReadAsStringAsync();
+
+            var companies = JsonSerializer.Deserialize<IEnumerable<CompanyDto>>(res, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return companies!;
+
         }
 
         private async Task<IEnumerable<CompanyDto>?> SimpleGetAsync2()
