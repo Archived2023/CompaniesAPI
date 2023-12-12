622aa6c4a8d535bf2db1f841b0b31294be10291b
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-12-12 13:34:57 +0100

    EmpService

---
+++ Companies.EmployeeService/EmpService.cs
@@ -0,0 +1,39 @@
+﻿namespace Companies.EmployeeService
+{
+    public class EmpService
+    {
+        private readonly IValidator validator;
+
+        public EmpService(IValidator validator)
+        {
+            this.validator = validator;
+        }
+
+        public bool RegisterUser(Employee employee)
+        {
+            //Do alot of stuff...
+            return validator.ValidateName(employee);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Salary { get; set; }
+        public SalaryLevel salaryLevel { get; set; }
+    }
+
+    public enum SalaryLevel
+    {
+        Default,
+        NotSet,
+        Junior,
+        Senior
+    }
+
+    public interface IValidator
+    {
+        SalaryLevel ValidateSalaryLevel(Employee employee);
+        bool ValidateName(Employee employee);
+    }
+}
