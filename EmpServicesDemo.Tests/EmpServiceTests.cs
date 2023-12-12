20651457317a077644ad23d0b356de1436e4008c
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-12-12 14:18:30 +0100

    Better and simplified syntax

--- EmpServicesDemo.Tests/EmpServiceTests.cs
+++ EmpServicesDemo.Tests/EmpServiceTests.cs
@@ -29,17 +29,21 @@ namespace EmpServicesDemo.Tests
         [Fact]
         public void HandleMessage_ShouldReturnTrueIfMatch()
         {
-            var iMessageMock = new Mock<IMessage>();
-            iMessageMock.Setup(x => x.Message).Returns("Text");
+            const string expectedText = "Text";
+            //var iMessageMock = new Mock<IMessage>();
+            //iMessageMock.Setup(x => x.Message).Returns("Text");
 
-            var iHandlerMock = new Mock<IHandler>();
-            iHandlerMock.Setup(x => x.CheckMessage).Returns(iMessageMock.Object);
+            //var iHandlerMock = new Mock<IHandler>();
+            //iHandlerMock.Setup(x => x.CheckMessage).Returns(iMessageMock.Object);
+
+            //var mockValidator = new Mock<IValidator>();
+            //mockValidator.Setup(x => x.Handler).Returns(iHandlerMock.Object);
 
             var mockValidator = new Mock<IValidator>();
-            mockValidator.Setup(x => x.Handler).Returns(iHandlerMock.Object);
+            mockValidator.Setup(x => x.Handler.CheckMessage.Message).Returns(expectedText);
 
             var sut = new EmpService(mockValidator.Object);
-            var actual = sut.HandleMessage("Text");
+            var actual = sut.HandleMessage(expectedText);
 
             Assert.True(actual);
         }
