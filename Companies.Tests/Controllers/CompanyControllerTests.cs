1869e0d51f4ec51a1de1070ecf3e4a95a5a6bc50
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-12-13 15:24:20 +0100

    CompaniesControllerTests

---
+++ Companies.Tests/Controllers/CompanyControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Companies.API.Dtos.CompaniesDtos;
+using Companies.API.Dtos.EmployeesDtos;
+using Companies.Tests.Fixtures;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Companies.Tests.Controllers
+{
+    public class CompanyControllerTests : IClassFixture<CompaniesControllerFixture>
+    {
+        private readonly CompaniesControllerFixture fixture;
+
+        public CompanyControllerTests(CompaniesControllerFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public async Task GetCompany_ShouldReturnAllCompanies()
+        {
+            var comp = GetCompanys();
+            fixture.CompanyService.Setup(x => x.GetAsync(false)).ReturnsAsync(comp);
+
+            var output = await fixture.Controller.GetCompany();
+
+            var okResult = Assert.IsType<OkObjectResult>(output.Result);
+            var items = Assert.IsType<List<CompanyDto>>(okResult.Value);
+            Assert.Equal(items.Count, comp.Count);
+        }
+
+        private List<CompanyDto> GetCompanys()
+        {
+            return new List<CompanyDto>
+            {
+                new CompanyDto
+                {
+                     Id = Guid.NewGuid(),
+                     Name = "Test",
+                     Address = "Ankeborg, Sweden",
+                     Employees = new List<EmployeeDto>()
+                },
+                 new CompanyDto
+                {
+                     Id = Guid.NewGuid(),
+                     Name = "Test",
+                     Address = "Ankeborg, Sweden",
+                     Employees = new List<EmployeeDto>()
+                }
+            };
+
+        }
+    }
+}
---
+++ Companies.Tests/Fixtures/CompaniesControllerFixture.cs
@@ -0,0 +1,25 @@
+﻿using Companies.API.Controllers;
+using Companies.API.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companies.Tests.Fixtures
+{
+    public class CompaniesControllerFixture 
+    {
+        public Mock<ICompanyService> CompanyService { get; }
+        public CompaniesController Controller { get; }
+        public CompaniesControllerFixture()
+        {
+            CompanyService = new Mock<ICompanyService>();
+            var serviceManager = new Mock<IServiceManager>();
+            serviceManager.Setup(x => x.CompanyService).Returns(CompanyService.Object);
+            Controller = new CompaniesController(serviceManager.Object);
+        }
+
+    }
+}
