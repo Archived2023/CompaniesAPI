d8310021b3d5be59deb9158163f359683c584b3b
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-12-13 15:56:57 +0100

    With TestDataBase

---
+++ Companies.Tests/Controllers/EmployeeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Companies.API.Controllers;
+using Companies.Tests.Fixtures;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companies.Tests.Controllers
+{
+    public class EmployeeControllerTests :IClassFixture<TestDataBaseFixture>
+    {
+        private readonly TestDataBaseFixture fixture;
+        private readonly EmployeesController sut;
+
+        public EmployeeControllerTests(TestDataBaseFixture fixture)
+        {
+            this.fixture = fixture;
+            sut = new EmployeesController(fixture.Context, fixture.Mapper);
+        }
+
+        [Fact]
+        public async Task GetEmployee_CompanyDoesentExist_ShouldReturnNotFound()
+        {
+            var output = sut.GetEmployee(Guid.NewGuid());
+            Assert.NotNull(output);
+            var res = output.Result.Result;
+            Assert.IsType<NotFoundResult>(res);
+        }
+    }
+}
---
+++ Companies.Tests/Fixtures/TestDataBaseFixture.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Companies.API.Data;
+using Companies.API.Entities;
+using Companies.API.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companies.Tests.Fixtures
+{
+    public class TestDataBaseFixture : IDisposable
+    {
+        public Mapper Mapper { get; private set; }
+        public APIContext Context { get; }
+
+        public TestDataBaseFixture()
+        {
+            Mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<EmployeeMappings>();
+            }));
+
+            var options = new DbContextOptionsBuilder<APIContext>()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TestDB;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+
+            Context = new APIContext(options);
+            Context.Database.Migrate();
+
+            Context.Companies.AddRange(new[]
+          {
+                new Company
+                {
+                    Name = "TestComp",
+                     Address = "TestAddres",
+                     Country = "Sweden",
+                      Employees = new[]
+                      {
+                          new Employee
+                          {
+                               Name = "Pelle",
+                                Age = 50,
+                                 Department = new Department
+                                 {
+                                      Name = "Developer"
+                                 }
+                          },
+                          new Employee
+                          {
+                               Name = "Nisse",
+                                Age = 40,
+                                 Department = new Department
+                                 {
+                                      Name = "Tester"
+                                 }
+                          },
+                      }
+                }
+            });
+
+            Context.SaveChanges();
+
+
+        }
+
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+            Context.Dispose();
+        }
+    }
+}
