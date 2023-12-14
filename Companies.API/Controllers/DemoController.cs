86fc94edbae5ac41ecf4d2a79a364404039574f1
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-12-14 11:31:39 +0100

    First integration test

---
+++ Companies.API/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Companies.API.Controllers
+{
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("Working");
+        }
+    }
+}
---
+++ Companies.Integration.Tests/DemoControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Companies.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Companies.Integration.Tests
+{
+    public class DemoControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private HttpClient httpClient;
+
+        public DemoControllerTests(WebApplicationFactory<Program> applicationFactory)
+        {
+            applicationFactory.ClientOptions.BaseAddress = new Uri("https://localhost:7157/api/demo/");
+            httpClient = applicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Index_ShouldReturnOk()
+        {
+            var response = await httpClient.GetAsync("");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
