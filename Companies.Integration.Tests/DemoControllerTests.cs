ac79535c764a2dfcf11ec41322c80f29320d5696
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-12-14 11:43:48 +0100

    Somemore tests

--- Companies.Integration.Tests/DemoControllerTests.cs
+++ Companies.Integration.Tests/DemoControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using Companies.API;
+using Companies.API.Dtos.CompaniesDtos;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
+using System.Text.Json;
 
 namespace Companies.Integration.Tests
 {
@@ -33,6 +35,41 @@ namespace Companies.Integration.Tests
 
             Assert.Equal("Working", content);
             Assert.Equal("text/plain", response.Content.Headers.ContentType.MediaType);
+
+        }
+
+        [Fact]
+        public async Task Index2_ShouldReturnExpectedMediaType()
+        {
+            var response = await httpClient.GetAsync("dto");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var dto = JsonSerializer.Deserialize<CompanyDto>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            Assert.Equal("Working", dto.Name);
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+
+        }
+
+        [Fact]
+        public async Task Index3_ShouldReturnExpectedMessage_WithStream()
+        {
+            var response = await httpClient.GetStreamAsync("dto");
+
+            var dto = await JsonSerializer.DeserializeAsync<CompanyDto>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            Assert.Equal("Working", dto.Name);
+
+        }
+
+        [Fact]
+        public async Task Index4_ShouldReturnExpectedMessageSimplifyed()
+        {
+            var dto = await httpClient.GetFromJsonAsync<CompanyDto>("dto");
+            Assert.Equal("Working", dto.Name);
 
         }
 
